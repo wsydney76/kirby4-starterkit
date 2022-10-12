41ad8849d0b05bac9ff07231de388e95247d5689
wsydn <wsydney@web.de>
2022-10-12 20:28:29 +0200

    Initial commit

--- site/snippets/blocks/button.php
+++
@@ -1,4 +0,0 @@
-<div style="margin:24px 0;">
-  <a style="display:inline-block; background-color: #4271ae; color: white; padding: 12px 24px; border-radius: 999px"
-     href="<?= $block->link() ?>"><?= $block->text()->esc() ?></a>
-</div>
