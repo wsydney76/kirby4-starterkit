6953db00a4a07a3eac380750c9de2d2658a76a90
wsydn <wsydney@web.de>
2022-10-30 19:02:21 +0100

    Initial commit

--- web/index.php
+++ web/index.php
@@ -4,14 +4,17 @@ require '../vendor/autoload.php';
 
 echo (new Kirby([
     'roots' => [
-        'index'    => __DIR__,
-        'base'     => $base    = dirname(__DIR__),
-        'content'  => $base . '/content',
-        'config'  => $base . '/config',
-        'site'     => $base . '/site',
-        'storage'  => $storage = $base . '/storage',
+        'index' => __DIR__,
+        'base' => $base = dirname(__DIR__),
+        'content' => $base . '/content',
+        'config' => $config = $base . '/config',
+        'blueprints' => $config . '/blueprints',
+        'collections' => $config . '/collections',
+        'site' => $base . '/site',
+        'templates' => $base . '/templates',
+        'storage' => $storage = $base . '/storage',
         'accounts' => $storage . '/accounts',
-        'cache'    => $storage . '/cache',
+        'cache' => $storage . '/cache',
         'sessions' => $storage . '/sessions',
     ]
 ]))->render();
