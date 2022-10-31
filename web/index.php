87c5139ebb25ea00f5fc4daf7157cd6dbeabe2a4
wsydn <wsydney@web.de>
2022-10-31 09:23:25 +0100

    Initial commit

--- web/index.php
+++ web/index.php
@@ -6,14 +6,13 @@ echo (new Kirby([
     'roots' => [
         'index' => __DIR__,
         'base' => $base = dirname(__DIR__),
-        'content' => $base . '/content',
+        'accounts' => $base . '/data/accounts',
+        'content' => $base . '/data/content',
         'config' => $config = $base . '/config',
         'blueprints' => $config . '/blueprints',
-        'collections' => $config . '/collections',
         'site' => $base . '/site',
         'templates' => $base . '/templates',
         'storage' => $storage = $base . '/storage',
-        'accounts' => $storage . '/accounts',
         'cache' => $storage . '/cache',
         'sessions' => $storage . '/sessions',
     ]
