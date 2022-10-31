b28e3d52b94c0f2e9898f0aefac6fccb0b4be513
wsydn <wsydney@web.de>
2022-10-31 11:54:05 +0100

    Add .env support

--- config/config.php
+++ config/config.php
@@ -3,18 +3,23 @@
 /**
  * The config file is optional. It accepts a return array with config options
  * Note: Never include more than one return statement, all options go within this single return array
- * In this example, we set debugging to true, so that errors are displayed onscreen. 
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
  * This setting must be set to false in production.
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 
+\Beebmx\KirbyEnv::load('..');
+
+$isDev = env('ENVIRONMENT') === 'dev';
+
 return [
-	'debug' => true,
-	'panel.install' => true,
+    'debug' => $isDev,
+    'panel.install' => true,
     'thumbs' => [
         'format' => 'webp'
     ],
     'amteich.twig.env.extensions' => [
         'site' => 'site\\twigextensions\\SiteExtension'
-    ]
+    ],
+    'amteich.twig.cache' => !$isDev
 ];
