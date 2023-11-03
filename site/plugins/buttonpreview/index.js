6c10ebed09dee8115367b7f413042527bfd80b99
wsydn <wsydney@web.de>
2023-11-03 13:37:46 +0100

    Extended examples

--- site/plugins/buttonpreview/index.js
+++ site/plugins/buttonpreview/index.js
@@ -2,17 +2,32 @@ panel.plugin("kirby4-starterkit/buttonpreview-block", {
   blocks: {
     buttonpreview: {
       computed: {
-        placeholder() {
+        textPlaceholder() {
           return "Button text …";
+        },
+        linkPlaceholder() {
+          return "https://example.com";
         }
       },
       template: `
-        <input
-          type="text"
-          :placeholder="placeholder"
-          :value="content.text"
-          @input="update({ text: $event.target.value })"
-        />
+        <div class="wrapper">
+          <input
+            class="button"
+            type="text"
+            :placeholder="textPlaceholder"
+            :value="content.text"
+            @input="update({ text: $event.target.value })"
+          />
+
+          <input
+            v-if="content.text.length > 0"
+            class="link"
+            type="url"
+            :placeholder="linkPlaceholder"
+            :value="content.link"
+            @input="update({ link: $event.target.value })"
+          />
+      </div>
       `
     }
   }
