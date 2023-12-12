730fd3f98e01feac09c571d296ae4b4fd25b9c5a
wsydn <wsydney@web.de>
2023-12-12 13:46:21 +0100

    Refactor Twig functions also to filters

--- site/twigextensions/SiteExtension.php
+++ site/twigextensions/SiteExtension.php
@@ -6,6 +6,7 @@ namespace site\twigextensions;
 use Kirby\Toolkit\Html;
 use Twig\Extension\AbstractExtension;
 use Twig\Markup;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 /**
@@ -18,18 +19,40 @@ class SiteExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('tel', function($tel) {
-                return new Markup(Html::tel($tel), 'UTF-8');
-            }),
-            new TwigFunction('email', function($email) {
-                return new Markup(Html::email($email), 'UTF-8');
-            }),
-            new TwigFunction('a', function($url, $text) {
-                return new Markup(Html::a($url, $text), 'UTF-8');
-            }),
-            new TwigFunction('attr', function($attr) {
-                return Html::attr($attr);
-            })
+            new TwigFunction('tel', [$this, 'telFunction']),
+            new TwigFunction('email', [$this, 'emailFunction']),
+            new TwigFunction('a', [$this, 'aFunction']),
+            new TwigFunction('attr', [$this, 'attrFunction'])
         ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('tel', [$this, 'telFunction']),
+            new TwigFilter('email', [$this, 'emailFunction']),
+            new TwigFilter('a', [$this, 'aFunction']),
+            new TwigFilter('attr', [$this, 'attrFunction'])
+        ];
+    }
+
+    public function telFunction($tel)
+    {
+        return new Markup(Html::tel($tel), 'UTF-8');
+    }
+
+    public function emailFunction($email)
+    {
+        return new Markup(Html::email($email), 'UTF-8');
+    }
+
+    public function aFunction($url, $text)
+    {
+        return new Markup(Html::a($url, $text), 'UTF-8');
+    }
+
+    public function attrFunction($attr)
+    {
+        return new Markup(Html::attr($attr), 'UTF-8');
     }
 }
